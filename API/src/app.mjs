30baba0101e3566bf21fb0f8911d40e822abe595
Lucas Simões Pólvora <122774951+LucasSimoesPolvora@users.noreply.github.com>
2024-02-26 11:10:28 +0100

    Creating some routes and the research of all books

--- API/src/app.mjs
+++ API/src/app.mjs
@@ -2,13 +2,25 @@ import express from "express";
 
 import { books } from "./db/mock-books.mjs";
 
+import { success } from "./routes/helper.mjs";
 const app = express();
 const port = 3000;
 
 app.get("/", (req,res) => {
-    res.json(books)
+    
+    res.json(success())
+})
+
+app.get("/api/", (req,res) => {
+    res.redirect(`http://localhost:${port}/`)
 })
 
 app.listen(port, () => {
     console.log(`http://localhost:${port}`)
 })
+
+// Finding all books 
+import { productsRouter } from "./routes/findAllBooks.mjs"
+app.use("/api/books", productsRouter)
+
+// Finding books by ID
