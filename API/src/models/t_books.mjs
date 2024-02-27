53c164a1007ae4f341bde204ded3abe11230577a
Ethan Aymeric Schafstall <po01imj@eduvaud.ch>
2024-02-27 13:46:47 +0100

    Added sequelize models
    Added models for the review, user, wrote, publisher tables.

--- API/src/models/t_books.mjs
+++ API/src/models/t_books.mjs
@@ -1,8 +1,10 @@
 // https://sequelize.org/docs/v6/core-concepts/validations-and-constraints/
 // Defines a model for transforming the data from json (within the mock-product file) to a json format ready to be exported to mysql.
-const BookModel = (sequelize, DataTypes) => {
+
+
+const bookModel = (sequelize, DataTypes) => {
     return sequelize.define(
-        "Books",
+        "t_books",
         {
             id_book: {
                 type: DataTypes.INTEGER,
@@ -90,4 +92,149 @@ const BookModel = (sequelize, DataTypes) => {
     );
 };
 
-export { BookModel };
+const reviewModel = (sequelize, DataTypes) => {
+    return sequelize.define(
+        "t_reviews",
+        {
+            // Not sure if any additional info for foreign keys are needed
+            fk_book: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+            },
+            fk_user: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+            },
+            revDate: {
+                type: DataTypes.DATE,
+                allowNull: true,
+                validate: {
+                },
+            },
+            revComment: {
+                type: DataTypes.STRING(300),
+                allowNull: true,
+                validate: {
+                },
+            },
+            revRating: {
+                type: DECIMAL(3, 2),
+                allowNull: false,
+                validate: {
+                    isInt: {
+                        msg: "Use only numbers for the rating",
+                    },
+                    notEmpty: {
+                        msg: "The rating count can not be empty.",
+                    },
+                    notNull: {
+                        msg: "The rating is a required property",
+                    },
+                    min: {
+                        args: [0],
+                        msg: "The rating can not be below 0",
+                    },
+                    max: {
+                        args: [5],
+                        msg: "The rating can not be above 5.",
+                    },
+                },
+            },
+
+        },
+        {
+            timestamps: true,
+            createdAt: "created",
+            updatedAt: false,
+        }
+    );
+};
+
+const userModel = (sequelize, DataTypes) => {
+    return sequelize.define(
+        "t_users",
+        {
+            id_user: {
+                type: DataTypes.INTEGER,
+                primaryKey: true,
+                autoIncrement: true,
+            },
+            usePseudo: {
+                type: DataTypes.STRING(50),
+                allowNull: false,
+            },
+            usePassword: {
+                type: DataTypes.STRING(50),
+                allowNull: false,
+            },
+            useBookCount: {
+                type: DataTypes.INTEGER,
+                allowNull: true,
+            },
+            useReviewCount: {
+                type: DataTypes.INTEGER,
+                allowNull: true,
+            },
+        },
+        {
+            timestamps: true,
+            createdAt: "created",
+            updatedAt: false,
+        }
+    );
+};
+
+const wroteModel = (sequelize, DataTypes) => {
+    return sequelize.define(
+        "t_wrote",
+        {
+            fk_author: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+            },
+            fk_book: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+            },
+
+        },
+        {
+            timestamps: true,
+            createdAt: "created",
+            updatedAt: false,
+        }
+    );
+};
+
+const publisherModel = (sequelize, DataTypes) => {
+    return sequelize.define(
+        "t_publishers",
+        {
+            id_publisher: {
+                type: DataTypes.INTEGER,
+                primaryKey: true,
+                autoIncrement: true,
+            },
+            pubName: {
+                type: DataTypes.STRING(50),
+                allowNull: false,
+            },
+
+        },
+        {
+            timestamps: true,
+            createdAt: "created",
+            updatedAt: false,
+        }
+    );
+};
+
+const apartModel = (sequelize, DataTypes) => {
+// A faire pour Lucas
+};
+
+const categoryModel = (sequelize, DataTypes) => {
+// A faire pour Lucas
+};
+
+export { bookModel, reviewModel, userModel, wroteModel, publisherModel, apartModel, categoryModel };
