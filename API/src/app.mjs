fbdd299bf8e4e20c43253975f697543fef1dd135
Ethan Aymeric Schafstall <po01imj@eduvaud.ch>
2024-03-02 09:06:24 +0100

    Added comments

--- API/src/app.mjs
+++ API/src/app.mjs
@@ -8,35 +8,63 @@ import { initDb } from "./db/sequelize.mjs";
 import swaggerUi from "swagger-ui-express";
 import { swaggerSpec } from "./swagger.mjs";
 
+import { allBooksRouter } from "./routes/getAllBooks.mjs"
+import { bookByIdRouter } from "./routes/getBookById.mjs"
 
 const app = express();
 const port = 3000;
 
 initDb();
 
+/**
+ * Handles GET requests to the root endpoint ("/").
+ * Responds with a JSON object indicating a successful response.
+ * @param {object} req - The request object.
+ * @param {object} res - The response object.
+ */
 app.get("/", (req,res) => {
     
     res.json(success())
 })
 
+/**
+ * Redirects requests to the root URL.
+ * @param {string} path - The path of the incoming request.
+ * @param {Object} reqBody - The request body object.
+ * @param {Object} resBody - The response body object.
+ */
 app.get("/api/", (req,res) => {
     res.redirect(`http://localhost:${port}/`)
 })
 
+/**
+ * Starts the server and listens for incoming connections on the specified port.
+ * Once the server is running, logs the URL where it's accessible.
+ * @param {number} port - The port number on which the server should listen.
+ */
 app.listen(port, () => {
     console.log(`http://localhost:${port}`)
 });
 
+/**
+ * Mounts Swagger UI middleware at the specified route.
+ * This middleware serves the Swagger UI interface, allowing users to interact with API documentation.
+ * @param {string} "/api-docs" - The route where the Swagger UI will be accessible.
+ * @param {function} swaggerUi.serve - Middleware function that serves Swagger UI assets.
+ * @param {function} swaggerUi.setup(swaggerSpec, options) - Middleware function that sets up Swagger UI with provided Swagger specification and options.
+ * @param {object} swaggerSpec - The Swagger specification object describing the API.
+ * @param {object} { explorer: true } - Options object passed to customize Swagger UI behavior (e.g., enabling the explorer).
+ */
 app.use(
     "/api-docs",
     swaggerUi.serve,
     swaggerUi.setup(swaggerSpec, { explorer: true })
 );
 
-// Finding all books 
-import { allBooksRouter } from "./routes/getAllBooks.mjs"
+/**
+ * Mounts routers for handling book-related API endpoints under the "/api/books" route.
+ * Two different routers are mounted under the same route: `allBooksRouter` and `bookByIdRouter`.
+ * Requests to "/api/books" will be processed by these routers based on their defined routes.
+ */
 app.use("/api/books", allBooksRouter)
-
-// Finding books by ID
-import { bookByIdRouter } from "./routes/getBookById.mjs"
 app.use("/api/books", bookByIdRouter)
