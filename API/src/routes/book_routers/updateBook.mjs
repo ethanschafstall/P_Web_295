741576ebea3b15c76802e645536853f124f6af18
Lucas Simões Pólvora <122774951+LucasSimoesPolvora@users.noreply.github.com>
2024-03-02 10:15:35 +0100

    Fix some issues

---
+++ API/src/routes/book_routers/updateBook.mjs
@@ -0,0 +1,11 @@
+import express from "express";
+import { books } from "../../db/mock-data.mjs";
+import { success,failure } from "../helper.mjs";
+
+const updateBookRouter = express() 
+
+updateBookRouter.post("/:id", (req, res) => {
+    
+})
+
+export { updateBookRouter }
