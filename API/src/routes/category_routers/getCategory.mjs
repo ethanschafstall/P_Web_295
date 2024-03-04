7bd57a872065c83969b99c06c420fc07ce55d784
Lucas Simões Pólvora <122774951+LucasSimoesPolvora@users.noreply.github.com>
2024-03-04 11:08:33 +0100

    Change some message spelling errors 2

--- API/src/routes/category_routers/getCategory.mjs
+++ API/src/routes/category_routers/getCategory.mjs
@@ -8,14 +8,14 @@ getCategoryRouter.get("/:id", (req, res) => {
     Category.findByPk(req.params.id)
         .then((category) => {
             if(category === null){
-                const message = `Le produit demandé n'existe pas. Merci de réessayer avec un autre identifiant.`
+                const message = `a catégorie demandé n'existe pas. Merci de réessayer avec un autre identifiant.`
                 return res.status(404).json({ message })
             }
-            const message = `Le produit dont l'id vaut ${category.id_category} a bien été récupéré`
+            const message = `Le catégorie dont l'id vaut ${category.id_category} a bien été récupéré`
             res.json(success(message, category))
         })
         .catch((error) => {
-            const message = `Le produit n'a pas pu être récupéré. Merci de réessayer dans quelques instants`;
+            const message = `Le catégorie n'a pas pu être récupéré. Merci de réessayer dans quelques instants`;
             res.status(500).json({message, data: error})
         })
 })
