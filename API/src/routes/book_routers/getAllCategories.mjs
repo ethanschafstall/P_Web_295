a09b2a5e3bf4352b6956cfc38cfc3ec47cff96bc
Lucas Simões Pólvora <122774951+LucasSimoesPolvora@users.noreply.github.com>
2024-03-04 10:24:37 +0100

    Make the get all categories file

---
+++ API/src/routes/book_routers/getAllCategories.mjs
@@ -0,0 +1,19 @@
+import express from "express";
+import { Category } from "../../db/sequelize.mjs";
+import { success } from "../helper.mjs";
+
+const getAllCategoriesRouter = express();
+
+getAllCategoriesRouter.get("/", (req, res) => {
+    Category.findAll({})
+        .then((category) => {
+            const message = `La liste des produits a bien été récupéré`;
+            res.json(success(message, category))
+        })
+        .catch((error) => {
+            const message = `La liste de produits n'a pas pu être récupérée. Merci de réessayer dans quelques instants.`
+            res.status(500).json({message, data: error})
+        })
+})
+
+export { getAllCategoriesRouter }
