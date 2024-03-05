b78450aec85478f3b4d19e257ee7ae11d770cbce
Lucas Simões Pólvora <122774951+LucasSimoesPolvora@users.noreply.github.com>
2024-03-05 14:04:12 +0100

    Add the query by publishers

--- API/src/routes/publisher_routes/getAllPublishers.mjs
+++ API/src/routes/publisher_routes/getAllPublishers.mjs
@@ -1,10 +1,20 @@
 import express from "express";
 import { Publisher } from "../../db/sequelize.mjs";
 import { success } from "../helper.mjs";
+import { ValidationError, Op } from "sequelize";
 
 const getAllPublishersRouter = express();
 
 getAllPublishersRouter.get("/", (req, res) => {
+    if(req.query.name) {
+        return Publisher.findAll({
+            where: { name: { [Op.like]: `%${req.query.name}&`}}
+        })
+        .then((publisher) => {
+            const message = `Il y a ${publisher.length} livres qui correspondent au terme de la recherche`;
+            res.json(success(message, publisher))
+        })
+    }
     Publisher.findAll({})
         .then((publisher) => {
             const message = `La liste des éditeurs a bien été récupéré`;
