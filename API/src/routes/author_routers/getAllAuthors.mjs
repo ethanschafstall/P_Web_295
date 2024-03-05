b4cf50f284b2938bf5bc854d9ce7a2705d43ed29
Lucas Simões Pólvora <122774951+LucasSimoesPolvora@users.noreply.github.com>
2024-03-05 14:03:47 +0100

    Add the query by name of the authors

--- API/src/routes/author_routers/getAllAuthors.mjs
+++ API/src/routes/author_routers/getAllAuthors.mjs
@@ -1,10 +1,20 @@
 import express from "express";
 import { Author } from "../../db/sequelize.mjs";
 import { success } from "../helper.mjs";
+import { ValidationError, Op } from "sequelize";
 
 const getAllAuthorsRouter = express();
 
 getAllAuthorsRouter.get("/", (req, res) => {
+    if(req.query.name) {
+        return Author.findAll({
+            where: { name: { [Op.like]: `%${req.query.name}&`}}
+        })
+        .then((author) => {
+            const message = `Il y a ${author.length} livres qui correspondent au terme de la recherche`;
+            res.json(success(message, author))
+        })
+    }
     Author.findAll({})
         .then((author) => {
             const message = `La liste des auteurs a bien été récupéré`;
