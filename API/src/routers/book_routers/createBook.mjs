98317d10d6e26773926fa4082c46804fd2463a75
Lucas Simões Pólvora <122774951+LucasSimoesPolvora@users.noreply.github.com>
2024-03-12 16:11:38 +0100

    Make the swagger for the post book

--- API/src/routers/book_routers/createBook.mjs
+++ API/src/routers/book_routers/createBook.mjs
@@ -6,6 +6,97 @@ import { ValidationError } from "sequelize"; // Importing ValidationError from s
 
 const createBookRouter = express(); // Creating a new instance of express router
 
+/**
+ * @swagger
+ * /api/books:
+ *   post:
+ *     tags: [Book]
+ *     security:
+ *       - bearerAuth: []
+ *     summary: Créer un livre.
+ *     description: Créer un livre.
+ *     responses:
+ *       200:
+ *         description: Créer un livre.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     id_book:
+ *                       type: integer
+ *                       description: L'ID du livre.
+ *                       example: 1
+ *                     booTitle :
+ *                       type: string
+ *                       description: Le titre du livre.
+ *                       example: Sun 
+ *                     booPageCount:
+ *                       type: integer
+ *                       description: Le nom de l'auteur.
+ *                       example: 600
+ *                     booExtract:
+ *                       type: string
+ *                       description: Un extrait du livre.
+ *                       example: Il était une fois un informaticien qui vivait sous un bureau.
+ *                     booSummary:
+ *                       type: string
+ *                       description: Un résumé du livre.
+ *                       example: C'est l'histoire d'un informaticien.
+ *                     booAvgRating:
+ *                       type: float
+ *                       description: La note moyenne du livre.
+ *                       example: 4.5
+ *                     booCoverImage:
+ *                       type: string
+ *                       description: Un lien pour l'image de couverture du livre.
+ *                       example: https://th.bing.com/th/id/R.f999471f51eab473baa6fe5cf40a7f32?rik=CACwXQt6ldAzHQ&pid=ImgRaw&r=0
+ *                     booPublishDate:
+ *                       type: date
+ *                       description: La date de publication du livre.
+ *                       example: 11/11/2023
+ *                     fk_user:
+ *                       type: integer
+ *                       description: id de l'user qui a publié.
+ *                       example: 1
+ *                     fk_publisher:
+ *                       type: integer
+ *                       description: id de l'éditeur qui a publié.
+ *                       example: 1
+ *                     fk_category:
+ *                       type: integer
+ *                       description: id de l'categorie à qui il appartient.
+ *                       example: 1
+ *       401:
+ *         description: Pas de jeton d'authentification.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: object
+ *                   properties:
+ *                     message:
+ *                       type: string
+ *                       example:  Vous n'avez pas fourni de jeton d'authentification. Ajoutez-en un dans l'en-tête de la requête.
+ *       500:
+ *         description: erreur du serveur.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: object
+ *                   properties:
+ *                     message:
+ *                       type: string
+ *                       example:  L'auteur n'a pas pu être récupéré. Merci de réessayer dans quelques instants
+ */
 // Endpoint for creating a new book
 createBookRouter.post("/", auth,(req, res) => {
     // Creating a new book with the provided data
@@ -20,7 +111,7 @@ createBookRouter.post("/", auth,(req, res) => {
                 return res.status(400).json({ message: error.message, data: error });
             }
             // If any other error occurs during the process, return a generic error message
-            const message = "Le livren'a pas pu être ajouté. Merci de réessayer dans quelques instants.";
+            const message = "Le livre n'a pas pu être ajouté. Merci de réessayer dans quelques instants.";
             res.status(500).json({ message, data: error });
         });
 });
