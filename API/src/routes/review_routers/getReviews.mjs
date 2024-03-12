a7045043baa1fa4ea4a68f1bb80a988002b1ced5
ethanschafstall <ethans9999@gmail.com>
2024-03-12 10:30:46 +0100

    fixedGetReviews router

--- API/src/routes/review_routers/getReviews.mjs
+++ API/src/routes/review_routers/getReviews.mjs
@@ -2,37 +2,28 @@ import express from "express"; // Importing express for router creation
 import { Book, Review } from "../../db/sequelize.mjs"; // Importing Book and Review models from sequelize
 import { success } from "../helper.mjs"; // Importing success helper function
 import { auth } from "../../auth/auth.mjs"; // Importing auth middleware
+import { Op } from "sequelize"; // Importing ValidationError and Op from sequelize
 
-const getBookReviewsRouter = express(); // Creating a new instance of express router
+const getReviewsByBookRouter = express(); // Creating a new instance of express router
 
-getBookReviewsRouter.get("/:id/reviews", auth, (req, res) => {
-    const bookId = req.params.id;
-
-    // Finding the book by its ID
-    Book.findByPk(bookId)
-        .then((book) => {
-            console.log(book);
-            if (!book) {
-                // If the book with the provided ID is not found, return an error message
-                const message = `Aucun livre trouvé avec l'ID ${bookId}.`;
-                res.json(success(message));
-            } else {
-                // If the book is found, find its reviews
-                return Review.findAll({
-                    where: { id_book}
-                })
-                .then((reviews) => {
-                    // Returning success message along with the found reviews
-                    const message = `Les avis pour le livre "${book.name}" ont été récupérés avec succès.`;
-                    res.json(success(message));
-                })
-            }
-        })
-        .catch((error) => {
-            // If an error occurs during the process, return a generic error message
-            const message = `Les avis pour le livre avec l'ID ${bookId} n'ont pas pu être récupérés. Merci de réessayer dans quelques instants.`;
-            res.json(success(message));
-        });
+getReviewsByBookRouter.get("/:id/reviews", auth, (req, res) => {
+    if(req.params.id) {
+        return Book.findOne({
+            where: { id_book: { [Op.eq]: req.params.id } },
+        }).then((book) => {
+            Review.findAll({
+                where: { fk_book: { [Op.eq]: book.id_book } },
+            }).then((reviews) => {
+                if(reviews.length !=0){
+                    const message = `Voici tout les reviews du livre "${book.booTitle}"`;
+                    res.json(success(message, reviews));
+                }
+            });
+        }).catch((error) => {
+            const message = "La liste des reviews n'a pas pu être récupérée. Merci de réessayer dans quelques instants.";
+            res.status(500).json({ message, data: error });
+        })        
+    }
 });
 
-export { getBookReviewsRouter }; // Exporting the router for use in other files
+export { getReviewsByBookRouter }; // Exporting the router for use in other files
