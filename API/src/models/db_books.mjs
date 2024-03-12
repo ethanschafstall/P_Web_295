169f5b60959e227cb91d87aa6fb733a418a089be
ethanschafstall <ethans9999@gmail.com>
2024-03-12 16:30:32 +0100

    Updated db schema
    updated schema with validations.

--- API/src/models/db_books.mjs
+++ API/src/models/db_books.mjs
@@ -13,34 +13,101 @@ const bookModel = (sequelize, DataTypes) => {
             },
             booTitle: {
                 type: DataTypes.STRING(100),
+                allowNull: false,
+                validate: {
+                    notNull: {
+                        msg: "Le titre est requis",
+                    },
+                    len: {
+                        args: [1, 100],
+                        msg: "Le titre doit comporter entre 1 et 100 caractères",
+                    },
+                },
             },
             booPageCount: {
                 type: DataTypes.INTEGER,
+                allowNull: false,
+                validate: {
+                    notNull: {
+                        msg: "Le nombre de pages est requis",
+                    },
+                    min: {
+                        args: [1],
+                        msg: "Le nombre de pages doit être supérieur à 0",
+                    },
+                },
             },
-            // Not sure if i need to define a min/max string length in validate if notUrl isn't before.
-            booExtract: {
+            booExcerpt: {
                 type: DataTypes.STRING(300),
+                validate: {
+                    len: {
+                        args: [0, 300],
+                        msg: "L'extrait doit comporter au plus 300 caractères",
+                    },
+                },
             },
             booSummary: {
                 type: DataTypes.STRING(100),
+                validate: {
+                    len: {
+                        args: [0, 100],
+                        msg: "Le résumé doit comporter au plus 100 caractères",
+                    },
+                },
             },
             booAvgRating: {
-                type: DataTypes.DECIMAL(3,2),
+                type: DataTypes.DECIMAL(3, 2),
+                validate: {
+                    isDecimal: {
+                        msg: "La note moyenne doit être un nombre décimal",
+                    },
+                    min: {
+                        args: [0],
+                        msg: "La note moyenne doit être d'au moins 0",
+                    },
+                    max: {
+                        args: [5],
+                        msg: "La note moyenne doit être au plus 5",
+                    },
+                },
             },
             booCoverImage: {
                 type: DataTypes.STRING(300),
+                validate: {
+                    isUrl: {
+                        msg: "L'image de couverture doit être une URL valide",
+                    },
+                },
             },
             booPublishDate: {
                 type: DataTypes.DATE,
             },
             fk_user: {
                 type: DataTypes.INTEGER,
+                allowNull: false,
+                validate: {
+                    notNull: {
+                        msg: "L'identifiant de l'utilisateur est requis",
+                    },
+                },
             },
             fk_publisher: {
                 type: DataTypes.INTEGER,
+                allowNull: false,
+                validate: {
+                    notNull: {
+                        msg: "L'identifiant de l'éditeur est requis",
+                    },
+                },
             },
             fk_category: {
                 type: DataTypes.INTEGER,
+                allowNull: false,
+                validate: {
+                    notNull: {
+                        msg: "L'identifiant de la catégorie est requis",
+                    },
+                },
             },
         },
         {
@@ -57,18 +124,56 @@ const reviewModel = (sequelize, DataTypes) => {
         {
             fk_user: {
                 type: DataTypes.INTEGER,
+                allowNull: false,
+                validate: {
+                    notNull: {
+                        msg: "L'identifiant de l'utilisateur est requis",
+                    },
+                },
             },
             fk_book: {
                 type: DataTypes.INTEGER,
+                allowNull: false,
+                validate: {
+                    notNull: {
+                        msg: "L'identifiant du livre est requis",
+                    },
+                },
             },
             revDate: {
                 type: DataTypes.DATE,
+                allowNull: false,
+                validate: {
+                    notNull: {
+                        msg: "La date de l'avis est requise",
+                    },
+                },
             },
             revComment: {
                 type: DataTypes.STRING(300),
+                validate: {
+                    len: {
+                        args: [0, 300],
+                        msg: "Le commentaire doit comporter au maximum 300 caractères",
+                    },
+                },
             },
             revRating: {
                 type: DataTypes.DECIMAL(3, 2),
+                allowNull: false,
+                validate: {
+                    notNull: {
+                        msg: "La note est requise",
+                    },
+                    min: {
+                        args: [0],
+                        msg: "La note doit être d'au moins 0",
+                    },
+                    max: {
+                        args: [5],
+                        msg: "La note doit être au maximum de 5",
+                    },
+                },
             },
         },
         {
