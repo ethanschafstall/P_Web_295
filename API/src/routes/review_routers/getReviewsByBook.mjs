d2dc6133083db7c254a4cebd2b7ce6f0c2d76d9b
ethanschafstall <ethans9999@gmail.com>
2024-03-12 13:26:19 +0100

    Added comments

--- API/src/routes/review_routers/getReviewsByBook.mjs
+++ API/src/routes/review_routers/getReviewsByBook.mjs
@@ -6,29 +6,36 @@ import { Op } from "sequelize"; // Importing ValidationError and Op from sequeli
 
 const getReviewsByBookRouter = express(); // Creating a new instance of express router
 
+// Route to get reviews by book ID
 getReviewsByBookRouter.get("/:id/reviews", auth, (req, res) => {
+    // Check if the book ID parameter exists in the request
     if(req.params.id) {
+        // Find a record in the "Book" table where the book ID matches
         return Book.findOne({
             where: { id_book: { [Op.eq]: req.params.id } },
-            
         }).then((book) => {
+            // If no record is found for the book ID, return a 404 error
             if (book === null) {
                 const message = "Le livre demandé n'existe pas. Merci de réessayer avec un autre identifiant.";
                 return res.status(404).json({ message });
             }
+            // Find all reviews associated with the book ID
             Review.findAll({
                 where: { fk_book: { [Op.eq]: book.id_book } },
             }).then((reviews) => {
+                // If reviews are found, return them along with a success message
                 if(reviews.length !=0){
                     const message = `Voici tout les reviews du livre "${book.booTitle}"`;
                     res.json(success(message, reviews));
                 }
             });
         }).catch((error) => {
+            // If there's an error in fetching data, return a 500 error
             const message = "La liste des reviews n'a pas pu être récupérée. Merci de réessayer dans quelques instants.";
             res.status(500).json({ message, data: error });
         })        
     }
 });
+
 
 export { getReviewsByBookRouter }; // Exporting the router for use in other files
