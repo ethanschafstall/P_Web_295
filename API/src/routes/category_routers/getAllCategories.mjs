ba00ed03b0518831fc3e3a260ff1673da03f3ba7
ethanschafstall <ethans9999@gmail.com>
2024-03-12 11:25:00 +0100

    updated import

--- API/src/routes/category_routers/getAllCategories.mjs
+++ API/src/routes/category_routers/getAllCategories.mjs
@@ -2,7 +2,7 @@ import express from "express"; // Importing express for router creation
 import { Category } from "../../db/sequelize.mjs"; // Importing Category model from sequelize
 import { success } from "../helper.mjs"; // Importing success helper function
 import { auth } from "../../auth/auth.mjs"; // Importing auth middleware
-import { ValidationError, Op } from "sequelize"; // Importing ValidationError and Op from sequelize
+import { Op } from "sequelize"; // Importing ValidationError and Op from sequelize
 
 const getAllCategoriesRouter = express(); // Creating a new instance of express router
 
