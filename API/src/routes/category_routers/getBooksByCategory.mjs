03650bc7dcb829f3b9f15da2539e1c714ddbcb3a
ethanschafstall <ethans9999@gmail.com>
2024-03-12 13:31:53 +0100

    Added comments

--- API/src/routes/category_routers/getBooksByCategory.mjs
+++ API/src/routes/category_routers/getBooksByCategory.mjs
@@ -6,24 +6,31 @@ import { Op } from "sequelize"; // Importing ValidationError and Op from sequeli
 
 const getBooksByCategoryRouter = express(); // Creating a new instance of express router
 
+// Route to get books by category ID
 getBooksByCategoryRouter.get("/:id/books", auth, (req, res) => {
+    // Check if the category ID parameter exists in the request
     if(req.params.id) {
+        // Find a record in the "Category" table where the category ID matches
         return Category.findOne({
             where: { id_category: { [Op.eq]: req.params.id } },
         }).then((category) => {
-            if (cateogry === null) {
+            // If no record is found for the category ID, return a 404 error
+            if (category === null) {
                 const message = "Le categorie demandé n'existe pas. Merci de réessayer avec un autre identifiant.";
                 return res.status(404).json({ message });
             }
+            // Find all books associated with the category ID
             Book.findAll({
                 where: { fk_category: { [Op.eq]: category.id_category } },
             }).then((books) => {
+                // If books are found, return them along with a success message
                 if(books.length !=0){
                     const message = `Voici tout les livre ayant comme catégorie ${req.params.id}`;
                     res.json(success(message, books));
                 }
             });
         }).catch((error) => {
+            // If there's an error in fetching data, return a 500 error
             const message = "La liste des categories n'a pas pu être récupérée. Merci de réessayer dans quelques instants.";
             res.status(500).json({ message, data: error });
         })        
--- API/src/routes/user_routers/getBooksByUser.mjs
+++ API/src/routes/user_routers/getBooksByUser.mjs
@@ -6,24 +6,31 @@ import { Op } from "sequelize"; // Importing ValidationError and Op from sequeli
 
 const getBooksByUserRouter = express(); // Creating a new instance of express router
 
+// Route to get books by user ID
 getBooksByUserRouter.get("/:id/books", auth, (req, res) => {
+    // Check if the user ID parameter exists in the request
     if(req.params.id) {
+        // Find a record in the "User" table where the user ID matches
         return User.findOne({
             where: { id_user: { [Op.eq]: req.params.id } },
         }).then((user) => {
+            // If no record is found for the user ID, return a 404 error
             if (user === null) {
                 const message = "L'utilisateur demandé n'existe pas. Merci de réessayer avec un autre identifiant.";
                 return res.status(404).json({ message });
             }
+            // Find all books associated with the user ID
             Book.findAll({
                 where: { fk_user: { [Op.eq]: user.id_user } },
             }).then((books) => {
+                // If books are found, return them along with a success message
                 if(books.length !=0){
                     const message = `Voici tout les livres de l'utilisateur "${user.usePseudo}"`;
                     res.json(success(message, books));
                 }
             });
         }).catch((error) => {
+            // If there's an error in fetching data, return a 500 error
             const message = "La liste des livres n'a pas pu être récupérée. Merci de réessayer dans quelques instants.";
             res.status(500).json({ message, data: error });
         })        
