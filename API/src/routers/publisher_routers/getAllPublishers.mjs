e46841be66cb72b2aac574d48c3a1ceeece8df1e
LucasSimoesPolvora <122774951+LucasSimoesPolvora@users.noreply.github.com>
2024-03-12 22:37:09 +0100

    Make the swagger of get all publishers

--- API/src/routers/publisher_routers/getAllPublishers.mjs
+++ API/src/routers/publisher_routers/getAllPublishers.mjs
@@ -5,6 +5,66 @@ import { auth } from "../../auth/auth.mjs"; // Importing auth middleware
 
 const getAllPublishersRouter = express(); // Creating a new instance of express router
 
+/**
+ * @swagger
+ * /api/publishers:
+ *   put:
+ *     tags: [Category]
+ *     security:
+ *       - bearerAuth: []
+ *     summary: Rechercher toutes les catégories.
+ *     description: Rechercher toutes les catégories.
+ *     parameters:
+ *     responses:
+ *       200:
+ *         description: Rechercher les catégories.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     message:
+ *                       type: string
+ *                       example: La liste des éditeurs a bien été récupéré
+ *                     id_category:
+ *                       type: integer
+ *                       description: L'ID de la catégorie.
+ *                       example: 1
+ *                     catName:
+ *                       type: string
+ *                       description: Le nom de la catégorie.
+ *                       example: horreur
+ *       401:
+ *         description: Pas de jeton d'authentification.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: object
+ *                   properties:
+ *                     message:
+ *                       type: string
+ *                       example:  Vous n'avez pas fourni de jeton d'authentification. Ajoutez-en un dans l'en-tête de la requête.
+ *       500:
+ *         description: erreur du serveur.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: object
+ *                   properties:
+ *                     message:
+ *                       type: string
+ *                       example:  La liste des éditeurs n'a pas pu être récupérée. Merci de réessayer dans quelques instants.
+ */
+
 // Endpoint for getting all publishers
 getAllPublishersRouter.get("/", auth,(req, res) => {
     // Get all publishers
