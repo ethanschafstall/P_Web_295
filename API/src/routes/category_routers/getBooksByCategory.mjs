800a978b043bb4eba4b230469add4b3461c3c81f
ethanschafstall <ethans9999@gmail.com>
2024-03-12 10:30:26 +0100

    Create getBooksByCategory & getAlllReviews
    Created 2 new routers

---
+++ API/src/routes/category_routers/getBooksByCategory.mjs
@@ -0,0 +1,29 @@
+import express from "express"; // Importing express for router creation
+import { Book, Category } from "../../db/sequelize.mjs"; // Importing Book and Review models from sequelize
+import { success } from "../helper.mjs"; // Importing success helper function
+import { auth } from "../../auth/auth.mjs"; // Importing auth middleware
+import { Op } from "sequelize"; // Importing ValidationError and Op from sequelize
+
+const getBooksByCategoryRouter = express(); // Creating a new instance of express router
+
+getBooksByCategoryRouter.get("/:id/books", auth, (req, res) => {
+    if(req.params.id) {
+        return Category.findOne({
+            where: { id_category: { [Op.eq]: req.params.id } },
+        }).then((category) => {
+            Book.findAll({
+                where: { fk_category: { [Op.eq]: category.id_category } },
+            }).then((books) => {
+                if(books.length !=0){
+                    const message = `Voici tout les livre ayant comme catégorie ${req.params.id}`;
+                    res.json(success(message, books));
+                }
+            });
+        }).catch((error) => {
+            const message = "La liste des categories n'a pas pu être récupérée. Merci de réessayer dans quelques instants.";
+            res.status(500).json({ message, data: error });
+        })        
+    }
+});
+
+export { getBooksByCategoryRouter }; // Exporting the router for use in other files
