9c6b31854d7cb6d0fe20223c4ce5d4dc4cd934ac
ethanschafstall <ethans9999@gmail.com>
2024-03-12 14:10:45 +0100

    updated grammar

--- API/src/routes/book_routers/getBooksByCategory.mjs
+++ API/src/routes/book_routers/getBooksByCategory.mjs
@@ -14,7 +14,7 @@ getBooksByCategoryRouter.get("/:id/books", (req, res) => {
         }).then((category) => {
             // If no record is found for the category ID, return a 404 error
             if (category === null) {
-                const message = "Le categorie demandé n'existe pas. Merci de réessayer avec un autre identifiant.";
+                const message = "Le catégorie demandé n'existe pas. Merci de réessayer avec un autre identifiant.";
                 return res.status(404).json({ message });
             }
             // Find all books associated with the category ID
@@ -23,13 +23,13 @@ getBooksByCategoryRouter.get("/:id/books", (req, res) => {
             }).then((books) => {
                 // If books are found, return them along with a success message
                 if(books.length !=0){
-                    const message = `Voici tout les livre ayant comme catégorie ${req.params.id}`;
+                    const message = `Voici tout les livres ayant comme catégorie ${req.params.id}`;
                     res.json(success(message, books));
                 }
             });
         }).catch((error) => {
             // If there's an error in fetching data, return a 500 error
-            const message = "La liste des categories n'a pas pu être récupérée. Merci de réessayer dans quelques instants.";
+            const message = "La liste des catégories n'a pas pu être récupérée. Merci de réessayer dans quelques instants.";
             res.status(500).json({ message, data: error });
         })        
     }
