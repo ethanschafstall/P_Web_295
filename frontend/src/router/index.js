da633313e3d82833adc8db9e1b44dd34fdfba9dc
ethanschafstall <ethans9999@gmail.com>
2024-04-29 09:21:39 +0200

    simplified router, added new route

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import BookView from '@/views/BookView.vue'
 import LoginView from '@/views/LoginView.vue'
 
 const router = createRouter({
@@ -11,17 +12,14 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/books',
+      name: 'books',
+      component: BookView
     },
     {
       path: '/login',
       name: 'login',
-      component: () => import('../views/LoginView.vue')
+      component: LoginView
     }
   ]
 })
