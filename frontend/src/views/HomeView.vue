600b5dec413da7972f0c77c96c3a2aa80da60afd
ethanschafstall <ethans9999@gmail.com>
2024-04-29 09:20:26 +0200

    removed login from homeview

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,26 +1,10 @@
 <script setup>
 import axios from 'redaxios';
 
-const getData = async () => {
-  axios.get('http://localhost:3000/test16042024')
-    .then((answer) => {
-      console.log(answer.data)
-    })
-}
-
-const login = async () => {
-  axios.post('http://localhost:3000/login', {
-    nickname: 'toto',
-    password: 'titi'
-  }).then((answer) => {
-    console.log(answer.data)
-  })
-}
 </script>
 
 <template>
   <main>
-    <button @click="getData()">get</button>
-    <button @click="login()">login</button>
+
   </main>
 </template>
