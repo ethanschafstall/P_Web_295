8da3eca542c2b61f85f2a7d43fe0f925fa378f96
ethanschafstall <ethans9999@gmail.com>
2024-04-29 10:01:20 +0200

    added bookrow component

---
+++ frontend/src/components/BookRow.vue
@@ -0,0 +1,15 @@
+<!-- Component for displaying a row of books with arrow to navigate the row -->
+<script setup>
+defineProps({
+  books: {
+    type: Array,
+    required: true
+  }
+})
+</script>
+
+<template>
+</template>
+
+<style scoped>
+</style>
