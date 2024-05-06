aa12cced378fae4938e831aa2162c1bfad00daa1
Lucas Simões Pólvora <122774951+LucasSimoesPolvora@users.noreply.github.com>
2024-05-06 11:20:38 +0200

    Begin the display of the books (not done)

--- frontend/src/components/BookShow.vue
+++ frontend/src/components/BookShow.vue
@@ -3,7 +3,7 @@ import axios from 'redaxios'
 export default {
   data() {
     return {
-      
+      books: []
     }
   },
   mounted() {
@@ -14,8 +14,13 @@ export default {
       const APICall = 'http://localhost:3000/api/books'
       await axios.get(APICall, {
         withCredentials: true
-      }).then((response) => {
-        console.log(response)
+      }).then((result) => {
+        for (let i = 0; i < result.data.data.length; i++) {
+          if (result.data.data[i] !== null) {
+            this.books.push(result.data.data[i])
+            console.log(this.books)
+          }
+        }
       }).catch((error) => {
           console.error(error)
       })
@@ -25,9 +30,14 @@ export default {
 </script>
 
 <template>
-
+  <div v-for="book in books" class="bookDisplay">
+    <p>{{ book.booTitle }}</p>
+    <img :src="book.booCoverImage">
+  </div>
 </template>
 
 <style scoped>
-
+.bookDisplay img {
+  width: 100px;
+}
 </style>
