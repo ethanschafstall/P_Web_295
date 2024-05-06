d78a9bda3a349edd403a67b87749359a99e3ccf7
ethanschafstall <ethans9999@gmail.com>
2024-05-06 11:20:59 +0200

    added new search route

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import BooksView from '@/views/BooksView.vue'
 import LoginView from '@/views/LoginView.vue'
+import SearchView from '@/views/SearchView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +21,15 @@ const router = createRouter({
       path: '/login',
       name: 'login',
       component: LoginView
+    },
+    {
+      path: '/search/:filter?', // Remove "name" parameter
+      name: 'search',
+      props: route => ({
+        filter: route.params.filter || "", // Access filter parameter
+        q: route.query.q // Access name query parameter
+      }),
+      component: SearchView
     }
   ]
 })
