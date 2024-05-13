b7b6e5e972402afc6aef8ec7cd7721107c7270f1
Lucas Simões Pólvora <122774951+LucasSimoesPolvora@users.noreply.github.com>
2024-05-13 09:11:49 +0200

    Display the books with a different style

--- frontend/src/components/BookShow.vue
+++ frontend/src/components/BookShow.vue
@@ -3,41 +3,92 @@ import axios from 'redaxios'
 export default {
   data() {
     return {
-      books: []
+      books: [],
+      authors: []
     }
   },
   mounted() {
     this.getBooks()
+    this.getAuthorOfBook()
   },
   methods: {
     async getBooks() {
       const APICall = 'http://localhost:3000/api/books'
+      for (let i = 0; i < 3; i++) {
+        await axios.get(APICall, {
+          withCredentials: true
+        }).then((result) => {
+          for (let i = 0; i < result.data.data.length; i++) {
+            if (result.data.data[i] !== null) {
+              this.books.push(result.data.data[i])
+            }
+          }
+        }).catch((error) => {
+          console.error(error)
+        })
+      }
+    },
+    async getAuthorOfBook() {
+      const APICall = "http://localhost:3000/api/authors"
+
       await axios.get(APICall, {
         withCredentials: true
       }).then((result) => {
         for (let i = 0; i < result.data.data.length; i++) {
           if (result.data.data[i] !== null) {
-            this.books.push(result.data.data[i])
-            console.log(this.books)
+            this.authors.push(result.data.data[i])
           }
         }
+
+        this.books.forEach((book) => {
+          this.authors.forEach((author) => {
+            if (author.id_author == book.fk_publisher) {
+              book.author = `${author.autFirstName} ${author.autLastName}`
+            }
+          });
+        })
+
       }).catch((error) => {
-          console.error(error)
+        console.error(error)
       })
-    }
+    },
+    // redirectToBook(book){
+    //   let bookId = book.id_book
+      
+    //   location.href = "/book/" + bookId
+    // }
   }
 }
 </script>
 
 <template>
-  <div v-for="book in books" class="bookDisplay">
-    <p>{{ book.booTitle }}</p>
-    <img :src="book.booCoverImage">
+  <div v-for="book in books" class="bookSquare">
+    <div @click="redirectToBook(book)">
+      <img :src="book.booCoverImage">
+      <p class="title">{{ book.booTitle }}</p>
+      <p class="author">{{ book.author }}</p>
+      <p>Rating: {{ book.booAvgRating }}</p>
+    </div>
   </div>
 </template>
 
 <style scoped>
-.bookDisplay img {
-  width: 100px;
+img {
+  padding-top: 10px;
+  height: 200px;
+  max-width: 300px;
+}
+
+.bookSquare {
+  width: 300px;
+  border-radius: 10px;
+  border-color: rgb(65, 58, 58);
+  border-width: 1px;
+  border-style: groove;
+  margin-bottom: 50px
+}
+
+.bookSquare p {
+  margin-bottom: 5px;
 }
 </style>
--- frontend/src/views/BooksView.vue
+++ frontend/src/views/BooksView.vue
@@ -4,11 +4,16 @@ import BookShow from '@/components/BookShow.vue';
 </script>
 
 <template>
+  <h1>This is the book page</h1>
   <div class="books">
-    <h1>This is the book page</h1>
-    <BookShow/>
+    <BookShow />
   </div>
 </template>
 
 <style>
+.books {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+}
 </style>
