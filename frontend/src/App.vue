47d243f8d428c86238a2b1209f75565786326f38
Lucas Simões Pólvora <122774951+LucasSimoesPolvora@users.noreply.github.com>
2024-05-14 15:26:35 +0200

    Do 2 different scripts

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,31 @@
-<script>
+<script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import SearchBar from '@/components/SearchBar.vue'
 import axios from 'redaxios'
+</script>
 
+<script>
 export default {
+  data() {
+    return {
+      isConnected: false,
+    }
+  },
+  mounted() {
+    //this.isConnected()
+  },
   methods: {
-    isConnected(){
-      const APICall = 'http'
+    async isConnected() {
+      const APICall = 'http://localhost:3000/api/books'
+
+      await axios.get(APICall, {
+        withCredentials: true
+      }).then((result) => {
+        this.isConnected = true
+      }).catch((error) => {
+        this.isConnected = false
+      })
+
     }
   }
 }
@@ -21,7 +40,8 @@ export default {
           <div>
             <RouterLink :to="{ name: 'home' }">Accueil</RouterLink> |
             <RouterLink :to="{ name: 'books' }">Livres</RouterLink> |
-            <RouterLink :to="{ name: 'login' }">Compte</RouterLink>
+            <RouterLink v-if="!isConnected" :to="{ name: 'login' }">Login</RouterLink>
+            <RouterLink v-if="isConnected" to="/users">Compte</RouterLink>
           </div>
           <SearchBar></SearchBar>
         </nav>
@@ -44,24 +64,29 @@ export default {
   text-align: center;
   color: #2c3e50;
 }
+
 nav {
   padding: 30px 100px 30px 100px;
   display: flex;
   justify-content: space-around;
   align-items: center;
 }
+
 nav a {
   font-weight: bold;
   color: #7D7757;
 }
+
 nav a.router-link-exact-active {
   color: #447866;
 }
-nav{
+
+nav {
   color: #7D7757;
   font-size: 20px;
 }
-#content-zone{
+
+#content-zone {
   border: #87772F solid 3px;
   border-radius: 10px;
   width: 1200px;
@@ -69,7 +94,8 @@ nav{
   margin: auto;
   background-color: white;
 }
-footer{
+
+footer {
   height: 100px;
 }
 </style>
