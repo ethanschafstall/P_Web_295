a4b7be375539d4ec5434e9436307fd8ad14c88cb
Lucas Simões Pólvora <122774951+LucasSimoesPolvora@users.noreply.github.com>
2024-05-27 10:27:07 +0200

    Start the home view

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,11 +1,29 @@
 <!-- Vue for the home page -->
 <script setup>
+import lastBooks from '@components/LastBooks.vue'
 </script>
 
 <template>
   <main>
+    <div id="welcome">
+      <h1>Bienvenue</h1>
+      <p>Ce site, réalisé pour le projet web frontend (C-294), a été réalisé par Ethan Schafstall et Lucas Simoes
+        Polvora.
+        Réaliser pour apprendre à faire du frontend dynamique avec une base de donnée qui envoye des données et le
+        frontend,
+        codé en vue3. Ce site permet aux utilisateurs de publiés leurs livres et de mettres des informations
+        supplémentaires.
+      </p>
+    </div id="lastBooks">
+      
+    <div>
+
+    </div>
   </main>
 </template>
 
 <style>
+#welcome p {
+  font-size: 20px;
+}
 </style>
