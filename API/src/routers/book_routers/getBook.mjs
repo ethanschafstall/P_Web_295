b8a795bb05d22ed055ce179eaa637d68cfaff3e2
Lucas Simões Pólvora <122774951+LucasSimoesPolvora@users.noreply.github.com>
2024-05-27 09:28:31 +0200

    Allows to access the details on a book only if it's the user that published the book

--- API/src/routers/book_routers/getBook.mjs
+++ API/src/routers/book_routers/getBook.mjs
@@ -2,6 +2,9 @@ import express from "express"; // Importing express for router creation
 import { Book } from "../../db/sequelize.mjs"; // Importing Book model from sequelize
 import { success } from "../helper.mjs"; // Importing success helper function
 import { auth } from "../../auth/auth.mjs"; // Importing auth middleware
+import checkToken from "../../util/checkToken.mjs";
+import jwt from "jsonwebtoken"; // Importing jwt for token verification
+import { privateKey } from "../../auth/private_key.mjs";
 
 const getBookRouter = express(); // Creating a new instance of express router
 
@@ -34,7 +37,7 @@ const getBookRouter = express(); // Creating a new instance of express router
  *                   properties:
  *                     message:
  *                       type: string
- *                       example: Le livre dont l'id vaut 1 a bien été récupéré !  
+ *                       example: Le livre dont l'id vaut 1 a bien été récupéré !
  *                     id_book:
  *                       type: integer
  *                       description: L'ID du livre.
@@ -42,7 +45,7 @@ const getBookRouter = express(); // Creating a new instance of express router
  *                     booTitle :
  *                       type: string
  *                       description: Le titre du livre.
- *                       example: L'art de la guerre 
+ *                       example: L'art de la guerre
  *                     booPageCount:
  *                       type: integer
  *                       description: Le nom de l'auteur.
@@ -122,23 +125,43 @@ const getBookRouter = express(); // Creating a new instance of express router
 
 // Endpoint for getting a specific book by ID
 getBookRouter.get("/:id", auth, (req, res) => {
-    // Finding the book by its primary key (ID)
-    Book.findByPk(req.params.id)
-        .then((book) => {
-            // If the book doesn't exist, return 404 error
-            if(book === null){
-                const message = `Le livre demandé n'existe pas. Merci de réessayer avec un autre identifiant.`
-                return res.status(404).json({ message })
-            }
-            // If the book exists, return success message along with the book data
-            const message = `Le livre dont l'id vaut ${book.id_book} a bien été récupéré`
-            res.json(success(message, book))
-        })
-        .catch((error) => {
-            // If an error occurs during the process, return a generic error message
-            const message = `Le livre n'a pas pu être récupéré. Merci de réessayer dans quelques instants`;
-            res.status(500).json({message, data: error})
-        })
-})
+  // Finding the book by its primary key (ID)
+  Book.findByPk(req.params.id)
+    .then((book) => {
+      const authorizationHeader = String(req.headers["cookie"]);
+      const token = checkToken(authorizationHeader);
 
-export { getBookRouter } // Exporting the router for use in other files
+      // If the book doesn't exist, return 404 error
+      if (book === null) {
+        const message = `Le livre demandé n'existe pas. Merci de réessayer avec un autre identifiant.`;
+        return res.status(404).json({ message });
+      }
+      // Get the value of the token
+      jwt.verify(token, privateKey, (error, decodedToken) => {
+        if (error) {
+          // If token verification fails, return 401 Unauthorized status
+          const message = `L'utilisateur n'est pas autorisé à accéder à cette ressource`;
+          return res.status(401).json({ message });
+        }
+        // Extracting user ID from the decoded token
+        const userId = decodedToken.userId;
+        console.log(userId + " " + book.fk_user);
+        // Checking if the user ID in the request body matches the one in the token
+        if (book.fk_user != userId) {
+          // If user ID in the request body doesn't match the one in the token, return 401 Unauthorized status
+          const message = `L'identifiant de l'utilisateur est invalide`;
+          return res.status(401).json({ message });
+        }
+      });
+      // If the book exists, return success message along with the book data
+      const message = `Le livre dont l'id vaut ${book.id_book} a bien été récupéré`;
+      res.json(success(message, book));
+    })
+    .catch((error) => {
+      // If an error occurs during the process, return a generic error message
+      const message = `Le livre n'a pas pu être récupéré. Merci de réessayer dans quelques instants`;
+      res.status(500).json({ message, data: error + " " });
+    });
+});
+
+export { getBookRouter }; // Exporting the router for use in other files
